603c2b65b5d7a81e9de198cbfb0c60f98628eff2
leave3310 <sampisam3@gmail.com>
2021-09-14 23:18:49 +0800

    show todo word

---
+++ src/views/TodoList/TodoList.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.css";
+
+const TodoList = () => {
+  return (
+    <div className="layout">
+      <div>TodoList</div>
+    </div>
+  );
+};
+
+export default TodoList;
