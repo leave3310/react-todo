e59e549d2b63534277cc7fc325c5a0debf33dde0
leave3310 <sampisam3@gmail.com>
2021-09-14 13:59:46 +0800

    new add Item to todoList

--- src/hooks/useTodoList.ts
+++ src/hooks/useTodoList.ts
@@ -3,8 +3,18 @@ import { Todo } from "../types/todoList";
 
 const useTodoList = () => {
     const [todos, setTodos] = useState<Todo[]>([])
+
+    const addTodo = (todo: Todo) => {
+        setTodos([
+            ...todos,
+            todo
+        ])
+    }
+
     
-    return { todos } as const
+
+
+    return { todos, addTodo } as const
 }
 
 export default useTodoList
